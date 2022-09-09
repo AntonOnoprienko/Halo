40f31729134f3b743609b7c7d97dc9f2b7cdce63
AntonOnoprienko <onoprienkoanton88@gmail.com>
2022-09-10 00:29:22 +0300

    try 8

--- components/HeaderComponent.jsx
+++ components/HeaderComponent.jsx
@@ -2,51 +2,51 @@ import Image from "next/image";
 import { useState } from "react";
 import classes from "../styles/header.module.scss"
 
-export const HeaderComponent = (props) => {
+export const HeaderComponent = ({home,mission,places,team,work}) => {
 
 	const [isOpen,setIsOpen] = useState(false)
 
 	const scrolToSection = (refElement) => { 
-
 		window.scrollTo({
 			top: refElement.current.offsetTop,
 			behavior: 'smooth'
 		})
 	}
-
 	return (
 		<>
-		<div className={classes.header__wrapper}>
+			<section className={classes.header__wrapper}>
+				<h1 className='invisible'>Header</h1>
+				<a href="#"><Image src='/logotype.png' alt='logotype' width='110px' height='37px' /></a>
+			<ul className={classes.header__menu_items}>
+					<li onClick={() => { scrolToSection(home) }}><b>Home</b></li>
+					<li onClick={() => { scrolToSection(mission) }}>Our mission</li>
+					<li onClick={() => { scrolToSection(places) }}>Places</li>
+					<li onClick={() => { scrolToSection(team)}}>Team</li>	
+			</ul>
 
-			<Image src='/logotype.png' alt='logo' width='110px' height='37px' />
+				{!isOpen && <button className={classes.burgerBtn}
+					onClick={() => { setIsOpen(!isOpen) }}>
+					<Image src='/union.png' alt="union" width={22} height={17} />
+				</button>}
 
-			<div className={classes.header__menu_items}>
-					<b><span onClick={() => { scrolToSection(props.home) }}>Home</span></b>
-					<span onClick={() => { scrolToSection(props.mission) }}>Our mission</span>
-					<span onClick={() => { scrolToSection(props.places) }}>Places</span>
-					<span onClick={()=>{ scrolToSection(props.team)}}>Team</span>	
-			</div>
-			<div className={classes.burgerBtn} onClick={() => { setIsOpen(!isOpen)}}>
-				{ !isOpen && <Image src='/union.png' alt="union" width={22} height={17} />}
-				{ isOpen && <Image src='/icon-close.png' alt="union" width={16} height={16} />}
+				{isOpen && <button className={classes.burgerBtn}
+					onClick={() => { setIsOpen(!isOpen) }}>
+					<Image src='/icon-close.png' alt="union" width={16} height={16} />
+				</button>}
 
-			</div>
-			<div className={classes.header__button}>
-				<span>Apply</span>
-			
 
-				</div>
-				</div>
-			{isOpen && <div className={classes.navbar_wrapper}>
-				<p className={classes.navbar_item} style={{fontWeight:'700'}} onClick={() => { scrolToSection(props.home) }} >Home</p>
-				<p className={classes.navbar_item} onClick={() => { scrolToSection(props.work) }} >Our mission</p>
-				<p className={classes.navbar_item} onClick={() => { scrolToSection(props.places) }} >Places</p>
-				<p className={classes.navbar_item} onClick={()=> { scrolToSection(props.team)}}>Team</p>
-				<div className={classes.navbar_btn} onClick={() => {setIsOpen(false)}}>
-				<span>Apply</span>
-				</div>
-			</div>}
-			</>
+			<button type="button" className={classes.header__button}>Apply</button>
+			</section>
+			{isOpen && <ul className={classes.navbar_wrapper}>
+				<li className={classes.navbar_item} onClick={() => { scrolToSection(home) }} ><b>Home</b></li>
+				<li className={classes.navbar_item} onClick={() => { scrolToSection(work) }} >Our mission</li>
+				<li className={classes.navbar_item} onClick={() => { scrolToSection(places) }} >Places</li>
+				<li className={classes.navbar_item} onClick={() => { scrolToSection(team) }}>Team</li>
+				<li><button className={classes.navbar_btn}
+					onClick={() => { setIsOpen(false) }}>Apply</button>
+				</li>
+			</ul> }
+		</>
 	);
 }
 
--- components/carousel/Carouselitem.jsx
+++ components/carousel/Carouselitem.jsx
@@ -42,7 +42,6 @@ export const CarouselItem = ({ children }) => {
 		})
 		setOffset((currentOffset) => { 
 			const newOffset = currentOffset + PAGE_WIDTH
-			console.log(newOffset)
 			return Math.min(newOffset,PAGE_WIDTH * 2)
 		})
 	}
