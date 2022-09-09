977f2194d77bec17d183b968074cdfc943744e2c
AntonOnoprienko <onoprienkoanton88@gmail.com>
2022-09-09 03:04:43 +0300

    try_7

--- components/TeamComponent.jsx
+++ components/TeamComponent.jsx
@@ -12,7 +12,7 @@ export const TeamComponent = () => {
 			  <p className={classes.txt}>Learn more about how you can save our planets nature. </p>
 		  </div>
 		  <div className={classes.teamBig}>
-			  <Image src={'/team.png'} alt={'teamBig'} width={1120} height={435}/>
+			  <Image src={'/ourteam.png'} alt={'our team'} width={1120} height={435}/>
 		  </div>
 		  <div className={classes.teamSmall}>
 			  <Image src={'/teamSmall.png'} alt={'teamSmall'} width={350} height={605} />
